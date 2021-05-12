17a5182dbcc2b29ca52345e2d6b612ccaf183ccc
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-05-12 14:11:21 -0400

    cloudinary server set up

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cloudinary = require('cloudinary');
+const formData = require('express-form-data');
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,6 +18,28 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUD_API, 
+  api_secret: process.env.CLOUD_SECRET
+})
+
+app.use(cors({ 
+  origin: CLIENT_ORIGIN 
+})) 
+
+app.use(formData.parse())
+
+app.post('/image-upload', (req, res) => {
+
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+})
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/wandering',
