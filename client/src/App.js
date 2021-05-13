ba9ec1c5f19ddcfa5f226c77ac1f9e324f917778
Sean Kelly <invertedrdm@gmail.com>
2021-05-13 12:10:54 -0400

    here's a test login button

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Upload from "./components/Upload";
 import Navbar from "./components/Navbar";
+import LoginPage from "./views/logintest";
 import ProtectedRoute from "./auth/protected-route";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <div className="App">
       <Navbar />
       <Upload />
+      <LoginPage />
     </div>
   );
 };
---
+++ client/src/views/logintest.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthenticationButton from "../components/authentication-button";
+
+function LoginPage () {
+  return (
+    <div>
+        <AuthenticationButton />
+    </div>
+  )
+}
+
+export default LoginPage
