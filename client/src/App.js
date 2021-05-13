eae9c34963fa6ea2045ac7e1a49eed88295620b2
Sean Kelly <invertedrdm@gmail.com>
2021-05-13 11:38:42 -0400

    test login button works

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import AuthenticationButton from "./components/authentication-button";
 import './App.css';
 
 function App() {
