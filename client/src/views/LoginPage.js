94d8f9d337d16c59c5f293e7151992d9b5b5eff1
legit-jb <j_brennan@me.com>
2021-05-14 11:44:15 -0400

    added login page

---
+++ client/src/views/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AuthenticationButton from "../components/authentication-button";
+
+const LoginPage =() => {
+    
+    return (
+      <div>
+          <AuthenticationButton />
+      </div>
+    )
+  }
+  
+  export default LoginPage;
+
