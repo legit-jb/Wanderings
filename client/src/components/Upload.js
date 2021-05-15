f37fd0381c1d1f91d8935735780c7e5f1de191e7
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-05-15 17:48:12 -0400

    the form works oh god this stupid form

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,10 +1,22 @@
 import React, { useState } from 'react'
+import Coordinates from 'coordinate-parser'
+import API from '../utils/API'
+import Navbar from './Navbar'
 
 function Upload () {
   const [image, setImage] = useState('')
   const [url, setUrl] = useState('')
+  const [markerData, setMarkerData] = useState({
+    title: '',
+    image: '',
+    tags: '',
+    lat: '',
+    lon: '',
+    comments: ''
+  })
 
-  const uploadImage = () => {
+  const uploadImage = e => {
+    e.preventDefault()
     const data = new FormData()
     data.append('file', image)
     data.append('upload_preset', 'mutxq4jo')
@@ -16,29 +28,108 @@ function Upload () {
     })
       .then(res => res.json())
       .then(res => {
-        console.log (res);
-        setUrl(res.secure_url);
+        console.log(res)
+        setUrl(res.secure_url)
+        massageData(res)
       })
       .catch(err => console.log(err))
   }
 
+  const massageData = data => {
+    let rawLat = data.image_metadata.GPSLatitude
+    let rawLong = data.image_metadata.GPSLongitude
+
+    let lat1 = rawLat.replace('deg', '°')
+    let lon1 = rawLong.replace('deg', '°')
+
+    let coord = lat1 + lon1
+
+    let position = new Coordinates(coord)
+    let latitude = position.getLatitude()
+    let longitude = position.getLongitude()
+
+    console.log(latitude, longitude)
+
+    setMarkerData({
+      ...markerData,
+      image: data.secure_url,
+      lat: latitude,
+      lon: longitude
+    })
+  }
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    setMarkerData({
+      ...markerData,
+      [name]: value
+    })
+    console.log(markerData)
+  }
+
+  const saveImage = e => {
+    e.preventDefault()
+    console.log(markerData)
+
+    API.saveMarker({
+      title: markerData.title,
+      image: markerData.image,
+      tags: markerData.tags,
+      lat: markerData.lat,
+      lon: markerData.lon,
+      comments: markerData.comments
+    })
+      .then(() => console.log(markerData))
+      .catch(err => console.log(err))
+  }
+
   return (
-    <div className='card custom-card'>
-      <div className='card-body'>
-        <input
-          className='form-control p-2'
-          type='file'
-          onChange={e => setImage(e.target.files[0])}
-        />
-        <button className='btn custom-btn m-2' onClick={uploadImage}>
-          upload
-        </button>
-        <div className='container'>
-          <img src={url} />
+    <div>
+      <Navbar />
+      <div className='container'>
+        <div className='card custom-card'>
+          <form>
+            <input
+              className='form-control m-2'
+              type='file'
+              id='formFile'
+              onChange={e => setImage(e.target.files[0])}
+            />
+            <button className='btn custom-btn m-2' onClick={uploadImage}>
+              upload
+            </button>
+            <input
+              className='form-control m-2'
+              type='text'
+              name='title'
+              placeholder='title'
+              onChange={handleInputChange}
+            />
+            <input
+              className='form-control m-2'
+              type='text'
+              name='comments'
+              placeholder='caption (optional)'
+              onChange={handleInputChange}
+            />
+            <input
+              className='form-control m-2'
+              type='text'
+              name='tags'
+              placeholder='tags, separated by commas'
+              onChange={handleInputChange}
+            />
+            <button className='btn custom-btn m-2' onClick={saveImage}>
+              save
+            </button>
+          </form>
+          <div className='container'>
+            <img src={url} className='display-image' />
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
-export default Upload;
+export default Upload
