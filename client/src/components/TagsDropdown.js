06a9e59153d188f0f7a1a031fc616e0726aaac04
legit-jb <j_brennan@me.com>
2021-05-15 15:02:54 -0400

    fixed dropdown error

--- client/src/components/TagsDropdown.js
+++ client/src/components/TagsDropdown.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {fakeUser as user} from "../components/FakeDBUser";
 
 const TagsDropdown = (props) => {
     console.log("this is in tagsdropdown props ",props);
@@ -11,7 +10,7 @@ const TagsDropdown = (props) => {
               aria-labelledby='navbarDropdownMenuLink'
             >
                 {usertags.map ((tag) =>(
-                    <li className="tag-list-item clickable" key={user.id+tag}>{tag}</li>
+                    <li className="tag-list-item clickable" key={props.id+tag}>{tag}</li>
                 ))}
             </ul>
     )
