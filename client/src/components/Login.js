189ed14797f7a5a1d6d8e533e686248885a4a889
Sean Kelly <invertedrdm@gmail.com>
2021-05-16 20:05:19 -0400

    added login and switch button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from 'react'
+import API from '../utils/API'
 
 const Login = () => {
+  const [userData, setUserData] = useState({});
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const saveUser = async (event) => {
+    event.preventDefault();
+
+    await API.signIn(userData);
+    console.log(userData);
+  };
+
   return (
-    <div className='text-center'>
-      <h1>HELLLLLLLLOOOOOOOOO</h1>
+    <div>
+      <form id="signupForm">
+        <div className="row m-2">
+          <label for="formEmail" className="form-label col-sm-1 col-form-label">
+            Email
+          </label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="text"
+              id="formEmail"
+              name="email"
+              placeholder="email"
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="row m-2">
+          <label
+            for="formPassword"
+            className="form-label col-sm-1 col-form-label"
+          >
+            Password
+          </label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="password"
+              id="formPassword"
+              name="password"
+              placeholder="Password"
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <button className="btn custom-btn m-2" onClick={saveUser}>
+          Login!
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
 export default Login;
--- client/src/views/LoginPage.js
+++ client/src/views/LoginPage.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
-import API from '../utils/API';
 import Login from "../components/Login";
 import Signup from "../components/Signup";
 
 const LoginPage = () => {
   const [status, setStatus] = useState(true);
 
-  // if (status) {
-  //   return <Login />;
-  // }
-
-  return (
-    <div>
-      <Signup />
-    </div>
-  );
+  if (status) {
+    return (
+      <div>
+        <Login />
+        <button className="btn custom-btn m-2" onClick={() => setStatus(false)}>
+          Sign up instead!
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Signup />
+        <button className="btn custom-btn m-2" onClick={() => setStatus(true)}>
+          Login instead!
+        </button>
+      </div>
+    );
+  }
 };
 
 export default LoginPage;
