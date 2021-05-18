f7205f66883bb59a1ab906fbe170d53b1d25ec8c
legit-jb <j_brennan@me.com>
2021-05-17 22:34:55 -0400

    new context

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,64 +1,32 @@
-import React, { createContext, useEffect, setState, Component } from "react";
-// import {fakeUser as user} from "../components/FakeDBUser";
+import React, {useState, context, createContext, useEffect} from 'react';
 import API from "../utils/API";
 
 export const UserContext = createContext({
-  user: {},
-  search: "",
-});
-
-class UserContextProvider extends React.Component {
-  state = {
     user: {},
     search: "",
-  };
+  });
 
-  // getUser = async (email) => {
-  //   // event.preventDefault();
-  //   const user = await API.getEmail(email);
-  //   this.state(user.data[0]);
-  //   console.log("signup: heres the user ", user.data[0]);
-  // };
+const UserContextProvider = ({children}) =>{
+    const [user, setUser] = useState(null);
+    const [email, setEmail] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.search !== this.state.search) {
-      console.log("componentDidUpdate fired off")
-      // const userTemp = API.getEmail(this.state.search);
-      // this.setState ({user: API.getEmail(this.state.search)});
-      API.getEmail(this.state.search)
-      .then((res)=> {
-        console.log("UserContext: this is the .then return",res.data[0])
-      })
-      .then((res) => {
-        this.setState({user: res.data[0]})
-      })
-
+    // let setSearch = (email) => {
+    //     setSearch({ search: email });
+    //   };
+    function setSearch(email) {
+        setEmail(email);
     }
-  }
 
-  // setUser = (email) => {
-  //   const userTemp = API.getEmail(email);
-  //   this.setState ({user: userTemp});
-  // }
-
-  setSearch = (email) => {
-    this.setState({ search: email });
-  };
-
-  // setUser = (userObj) => {
-  //   this.setState({ user: userObj });
-  // };
-
-  render() {
-    return (
-      <UserContext.Provider value={{ ...this.state, setSearch: this.setSearch }}>
-        {this.props.children}
-      </UserContext.Provider>
-    );
-    // end return
-  }
-  // end render
+    useEffect(() =>{
+        API.getEmail(email).then(res => setUser(res.data))
+    },[email]);
+   
+   const { Provider } = UserContext
+   return(
+       <UserContext.Provider value={user,setSearch}>
+           {children}
+       </UserContext.Provider>
+   )
 }
-// end class
 
-export default UserContextProvider;
+export default UserContextProvider
